1f09df4d666b7c8b286be1a79d3db11e85ee955e
hai271120 <69028228+hai271120@users.noreply.github.com>
2020-11-15 19:20:03 +0700

    Add files via upload

--- ss 1/test.js
+++ ss 1/test.js
@@ -1,57 +1,14 @@
-
-// bai 1:
-//  let n = prompt("nhap vao n");
-// if(n>=4 && n <=20){
-//     console.log(n);
-// }else
-// {
-//     console.log("nhap lai n");
-// }
-// let k = prompt("nhap vao k ");
-
-// function timson(){
-//         kiemtran();
-//         console.log(kiemtran());
-//     let g=[];
-//     for (let j = 0; j < n-1; j++) {
-//         g.push(j);
-        
-//     }
-//     console.log(g);
-    
-// }
-// function findOppositeNumber() {
-//     let vitridoi= Number;
-//    timson(n);
-//    console.log(timson);
-//     for (let i = 0; i < g[i].length; i++) {
-//         if(k === g[i]){
-//             return i;
-//             vitridoi = i + ((n-1)/2);
-//             console.log(vitridoi);
-//         }
-        
-//     }
-// }
-// bai 2:
-function merge2String(a,b)
-{
-    c = String(a);
-    d = String(b);
-    e = [];
-    if (c.length > a.length)
-        for (let i = 0; i < a.length; i++) {
-            if (i<b.length)
-            e = e + a[i] + b[i];
-            else e = e + b[i]
+let n = Number(prompt('Nhập giá trị n thỏa mãn 4<=n<=20 và n chẵn:'));
+let k = Number(prompt('Nhập giá trị k thỏa mãn 0<= k <=(n-1):'));
+function findOppositeNumber(n,k) {
+    if ( n%2==0 & k>n & n>=4 & n<=20 ) {
+        return k-n/2;
     }
-    if (c.length <= b.length)
-        for (let i = 0; i < b.length; i++) {
-            if (i<a.length)
-            e = e + a[i] + b[i];
-            else e = e + b[i]
+    else if ( n%2==0 & k<=n & n>=4 & n<=20 ) {
+        return k+n/2;
     }
-    return e
+    else {
+        alert('Nhập lại giá trị thỏa mãn điều kiện đề bài.');
+    }
 }
-
-console.log(merge2String('abc','0123'))
+alert(findOppositeNumber(n,k));
